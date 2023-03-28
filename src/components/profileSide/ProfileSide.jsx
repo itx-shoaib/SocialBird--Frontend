e1a87a3393925b6976392aa2f80c945d7c2f99aa
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-03-29 01:31:09 +0500

    Serach Bar made

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LogoSearch from '../LogoSearch/LogoSearch'
 
 const ProfileSide = () => {
     return (
         <>
             <div className="profileSide">
-                ProfileSidein
+                <LogoSearch />
             </div>
         </>
     )
