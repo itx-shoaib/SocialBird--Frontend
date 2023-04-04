e10a6d1da38a90d1c335a58709621449e5799b61
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-04-05 02:59:44 +0500

    Making PostShare and PostSide components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ProfileSide from '../../components/profileSide/ProfileSide'
 import "./Home.css"
+import PostSide from '../../components/PostSide/PostSide'
 
 const Home = () => {
     return (
         <>
             <div className="Home">
                 <ProfileSide />
-                <div className="post">post</div>
+                <PostSide />
                 <div className="rightSide">rightSide</div>
             </div>
         </>
