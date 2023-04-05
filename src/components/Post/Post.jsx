e1d2d03d6657e3dbe8a38d4b520d501c3350b346
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-04-06 01:44:54 +0500

    Making Posts and post components

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Post.css"
+import Heart from "../../img/like.png"
+import notLike from "../../img/notlike.png"
+import Share from "../../img/share.png"
+import Comment from "../../img/comment.png"
+
+const Post = ({ data, id }) => {
+    return (
+        <div className="Post">
+            <img src={data?.img} alt="" />
+
+            <div className="postReact">
+                <img src={data?.liked ? Heart : notLike} alt="" />
+                <img src={Comment} alt="" />
+                <img src={Share} alt="" />
+            </div>
+
+            <span>{data?.likes} likes</span>
+
+            <div className="detail">
+                <span><b>{data?.name}</b></span>
+                <span>{data?.desc}</span>
+            </div>
+        </div>
+    )
+}
+
+export default Post
