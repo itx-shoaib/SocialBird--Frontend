a1831720014af3b9b9f166b4d5a1009cbffe7989
Shoaib Jamil <shoaibjamil43@gmail.com>
2023-06-18 02:41:40 +0500

    Making the uploadPost action and request

--- src/actions/UploadAction.js
+++ src/actions/UploadAction.js
@@ -7,3 +7,14 @@ export const uploadImage=(data)=>async(dispatch)=>{
         console.log(`Error in UploadAction/uploadImage. Error: ${error}`)
     }
 }
+
+export const uploadPost=(data)=>async(dispatch)=>{
+    dispatch({type:"UPLOAD_START"})
+    try {
+        const newPost = await UploadApi.uploadPost(data)
+        dispatch({type:"UPLOAD_SUCCESS"})
+    } catch (error) {
+        console.log(`Error in UploadAction/uploadPost. Error: ${error}`)
+        dispatch({type:"UPLOAD_FAIL"})
+    }
+}
